9eb6449136ffcfc3200641e23c964299bda87022
kumar-singh-au3 <manassingh26@gmail.com>
2021-01-18 12:21:19 +0530

    Updates

---
+++ precache-manifest.eda7dcf58f308edb863613edad758081.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1d2076dba14c24c622219e7c5d172fee",
+    "url": "/inkoopRickandMorty/index.html"
+  },
+  {
+    "revision": "b2ea61cc6694b67bf66d",
+    "url": "/inkoopRickandMorty/static/css/2.0e1a4c73.chunk.css"
+  },
+  {
+    "revision": "b2ea61cc6694b67bf66d",
+    "url": "/inkoopRickandMorty/static/js/2.48b94a29.chunk.js"
+  },
+  {
+    "revision": "7c3c85c36cf69f4567f9",
+    "url": "/inkoopRickandMorty/static/js/main.c831b0dc.chunk.js"
+  },
+  {
+    "revision": "f17906edcde89cea8141",
+    "url": "/inkoopRickandMorty/static/js/runtime~main.fcb80fd8.js"
+  }
+]);
